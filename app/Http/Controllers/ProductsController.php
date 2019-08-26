abcc948d7d6217b3d0d5306cef16d164649ca5d8
vagrant <vagrant@homestead>
2019-08-26 07:47:01 +0000

    用户界面 - 商品搜索、排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,8 +9,44 @@ class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        $products = Product::query()->where('on_sale', true)->paginate(16);
+        // 创建一个查询构造器
+        $builder = Product::query()->where('on_sale', true);
 
-        return view('products.index', compact('products'));
+        // search 参数用来模糊搜索商品
+        if ($search = $request->input('search', '')) {
+            $like = '%'.$search.'%';
+            // 模糊搜索商品标题、商品详情、SKU 标题、SKU描述
+            $builder->where(function ($query) use ($like) {
+                $query->where('title', 'like', $like)
+                        ->orWhere('description', 'like', $like)
+                        ->orWhereHas('skus', function ($query) use ($like) {
+                            $query->where('title', 'like', $like)
+                                ->orWhere('description', 'like', $like);
+                        });
+            });
+
+            /** $builder->where() 传入一个匿名函数，然后才在这个匿名函数里面再去添加 like 搜索，这样做目的是在查询条件的两边加上 ()，也就是说最终执行的 SQL 语句类似 select * from products where on_sale = 1 and ( title like xxx or description like xxx ) */
+        }
+
+        // order 参数用来控制商品的排序规则
+        if ($order = $request->input('order', '')) {
+            // 是否是以 _asc 或者 _desc 结尾
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                // 如果字符串的开头是这 3 个字符串之一，说明是一个合法的排序值
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    // 根据传入的排序值来构造排序参数
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+
+        $products = $builder->paginate(16);
+
+        $filters = [
+            'search' => $search,
+            'order' => $order,
+        ];
+
+        return view('products.index', compact('products', 'filters'));
     }
 }
