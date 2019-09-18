0b0cc359a3be9bdf81a8151fada560127231b266
vagrant <vagrant@homestead>
2019-09-18 06:21:02 +0000

    订单模型

---
+++ database/factories/OrdetItemFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\OrdetItem;
+use Faker\Generator as Faker;
+
+$factory->define(OrdetItem::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
