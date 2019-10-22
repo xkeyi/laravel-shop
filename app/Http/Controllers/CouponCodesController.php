014f40f6acf7bc679e574034df665e9e51ddb8d2
vagrant <vagrant@homestead>
2019-10-22 08:21:12 +0000

    用户界面 - 使用优惠券下单

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -10,27 +10,11 @@ class CouponCodesController extends Controller
 {
     public function show($code)
     {
-        // 判断优惠券是否存在
         if (!$record = CouponCode::where('code', $code)->first()) {
-            abort(404);
+            throw new CouponCodeUnavailableException('优惠券不存在');
         }
 
-        // 如果优惠券没有启用
-        if (!$record->enabled) {
-            abort(404);
-        }
-
-        if ($record->total - $record->used <= 0) {
-            return response()->json(['msg' => '该优惠券已被兑换完'], 403);
-        }
-
-        if ($record->not_before && $record->not_before->gt(Carbon::now())) {
-            return response()->json(['msg' => '该优惠券现在还不能使用'], 403);
-        }
-
-        if ($record->not_after && $record->not_after->lt(Carbon::now())) {
-            return response()->json(['msg' => '该优惠券已过期'], 403);
-        }
+        $record->checkAvailable();
 
         return $record;
     }
