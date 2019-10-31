b2c6a1762adf3ec27d7641ad50f8c57546581948
vagrant <vagrant@homestead>
2019-10-31 08:04:07 +0000

    众筹订单模块调整

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -60,8 +60,10 @@
         </div>
         @endif
 
-        <!-- 订单已支付，且退款状态不是未退款时展示退款信息 -->
-        @if($order->paid_at && $order->refund_status !== \App\Models\Order::REFUND_STATUS_PENDING)
+        <!-- 订单不是众筹订单，已支付，且退款状态不是未退款时展示退款信息 -->
+        @if($order->type !== \App\Models\Order::TYPE_CROWDFUNDING &&
+            $order->paid_at &&
+            $order->refund_status !== \App\Models\Order::REFUND_STATUS_PENDING)
         <div class="line">
           <div class="line-label">退款状态：</div>
           <div class="line-value">{{ \App\Models\Order::$refundStatusMap[$order->refund_status] }}</div>
