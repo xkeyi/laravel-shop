6184f2cf0c1f27951e07ca8dca708ec5d68c7be0
vagrant <vagrant@homestead>
2019-11-06 08:08:00 +0000

    创建分期付款

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,7 @@ Route::group([
 
     Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
+    Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
 
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 
